4730afd47052333bf0b8194e549789044048bd96
vedvy <vedant.vyas@stonybrook.edu>
2025-06-15 12:25:24 -0400

    figured out how to do npm run dev, now to look into how to structure the project before truly getting started

---
+++ my-app/src/app/layout.js
@@ -0,0 +1,14 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
