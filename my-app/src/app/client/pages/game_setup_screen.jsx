54bd60702031eee953775d83968c1b962780cbed
vedvy <vedant.vyas@stonybrook.edu>
2025-08-19 16:26:05 -0400

    added confirmation system foor deleting a player

--- my-app/src/app/client/pages/game_setup_screen.jsx
+++ my-app/src/app/client/pages/game_setup_screen.jsx
@@ -19,8 +19,13 @@ export default function SetUpScreen()
     the real-time stats updates for players and either discard for early quit or apply to all players if game is completed fully?). 
     Later on, add a button to prompt whether a 4th inning should be played (it sometimes happens.)*/
     const [current_step, set_current_step] = useState("AddDeletePage");
-    const [delete_page, update_delete_page] = useState(false);
     
+    const [delete_players_page, update_delete_players_page] = useState(false);
+    const [delete_details, update_delete_info] = useState({
+        confirmDelete: false,
+        player: ""
+    });
+
     const dataModel = useContext(SetUpScreenDataContext);
     if(!dataModel.loading)
     {
@@ -58,10 +63,11 @@ export default function SetUpScreen()
         return;
     }
     
+    console.log(delete_details.confirmDelete);
     return(
         <div className={styles.setUpScreenContainer}>
             {current_step === "AddDeletePage" && <div>
-               {!delete_page && <div className={styles.addPlayersSection}>
+               {!delete_players_page && <div className={styles.addPlayersSection}>
                 <h1>Add Players Page</h1>
                 {/*Add the input form here.*/}
                 <br/>
@@ -83,24 +89,34 @@ export default function SetUpScreen()
                 </form>
                 <br/>
                 <br/>
-                <button onClick={() => {update_delete_page(!delete_page)}} className={styles.pageButton}>{!delete_page ? "Delete Players" : "Add a Player"}</button>
+                <button onClick={() => {update_delete_players_page(!delete_players_page)}} className={styles.pageButton}>{!delete_players_page ? "Delete Players" : "Add a Player"}</button>
                 
             </div>}
-            {delete_page && <div className={styles.deletePlayersSection}>
+            {delete_players_page && <div className={styles.deletePlayersSection}>
                 <h1>Delete Players Page</h1>
                 <br/>
                 <h1>Current Players: </h1>
                 {dataModel.players.map((player, index) => 
-                <h3 key={index} className={styles.deletePlayersNames} onClick={() => {deletePlayer(player);}}>{player.name}</h3>)}
-                <button onClick={() => {update_delete_page(!delete_page)}} className={styles.pageButton}>{!delete_page ? "Delete Players" : "Add a Player"}</button>
+                <h3 key={index} className={styles.deletePlayersNames} onClick={() => {update_delete_info({confirmDelete: true, player: player});}}>{player.name}</h3>)}
+                <button onClick={() => {update_delete_players_page(!delete_players_page)}} className={styles.pageButton}>{!delete_players_page ? "Delete Players" : "Add a Player"}</button>
                 {/*Show mappings of players with names.*/}
-            
+                {delete_details.confirmDelete && 
+                <div>
+                    <br/>
+                    <br/>
+                    <h3>All statistics and data associated with {delete_details.player.name} will be deleted and gone forever.
+                    Are you sure you still want to delete this player?</h3>
+                    <br/>
+                    <button className={styles.quitSubmitButton} onClick={() => {update_delete_info({confirmDelete: false, player: ""}); deletePlayer(delete_details.player);}}>Yes, delete this player right now!</button>
+                    <button className={styles.quitCancelButton} onClick={() => {update_delete_info({confirmDelete: false, player: ""});}}>No thanks.</button>
+                </div>
+                }
             </div>}
             <br/>
             
              
             </div>}
-            
+            <button>NEXT PAGE</button>
         </div>
     )
 }
